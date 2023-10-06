55ee1d550f224bbd1f4c3809d55c845586344e43
TheFirstAvenger <mike@mikebinns.net>
2023-10-06 17:52:49 -0400

    v0.6.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule FlameOn.MixProject do
   def project do
     [
       app: :flame_on,
-      version: "0.5.2",
+      version: "0.6.0",
       elixir: "~> 1.15",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
